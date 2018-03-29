13fd8684442a88eb73b8a9dd47740d0650f151c6
SweetyKnowles <sweetyjames@hotmail.co.uk>
2018-03-29 15:09:29 -0400

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+//import Artist from "./components/Artist";
+import ArtistList from "./components/Artist";
+import SingleArtistView from "./components/SingleArtist";
+import NavBar from "./components/NaveBAr";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+
+                    <div>
+                        <h1>Tunr</h1>
+                        <div>
+                            <div><Link to="/">All Artists</Link></div>
+                        </div>
+                    </div>
+                    <Route exact path="/" component={NavBar}/>
+                    <Route exact path="/" component={ArtistList}/>
+                    <Route path="/artist/:id" component={SingleArtistView}/>
+
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ client/src/components/ArtistList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ArtistList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world </h1>
+            </div>
+        );
+    }
+}
+
+export default ArtistList;
